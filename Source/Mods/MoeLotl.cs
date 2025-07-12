e953cb668d8a8a814cc0a9650556b2069824564c
aliceandlamb <164008833+aliceandlamb@users.noreply.github.com>
2025-07-12 14:17:13 -0700

    Update MoeLotl.cs

--- Source/Mods/MoeLotl.cs
+++ Source/Mods/MoeLotl.cs
@@ -1,16 +1,15 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using Multiplayer.API;
 using Verse;
 
 namespace Multiplayer.Compat
 {
-    /// <summary>Alpha Mechs by Sarg Bjornson</summary>
-    /// <see href="https://github.com/juanosarg/AlphaMechs"/>
-    /// <see href="https://steamcommunity.com/sharedfiles/filedetails/?id=2973169158"/>
-    [MpCompatFor("HenTaiLoliTeam.Axolotl")]
-    public class Axolotl
+    /// <summary>MoeLotl Race by HenTaiLoliTeam </summary>
+    /// <see href="https://steamcommunity.com/workshop/filedetails/?id=3292351432"/>
+    [MpCompatFor("HenTaiLoliTeam.axolotl")]
+    public class MoeLotlRace
     {
-        public Axolotl(ModContentPack mod)
+        public MoeLotlRace(ModContentPack mod)
         {
             // Gizmos
             MP.RegisterSyncMethod(AccessTools.DeclaredMethod("Axolotl.CompProperties_LotiQiRangedWeapon_ChangeProjectile"));
