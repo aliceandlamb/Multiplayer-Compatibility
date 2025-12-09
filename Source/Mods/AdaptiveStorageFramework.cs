c39f7c10f5dad87200ebcea7e73a5ce2fd05c08f
aliceandlamb <164008833+aliceandlamb@users.noreply.github.com>
2025-12-09 15:02:28 -0800

    Update AdaptiveStorageFramework.cs

--- Source/Mods/AdaptiveStorageFramework.cs
+++ Source/Mods/AdaptiveStorageFramework.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Reflection;
 using HarmonyLib;
 using Multiplayer.API;
 using RimWorld;
@@ -7,20 +6,10 @@ using Verse;
 
 namespace Multiplayer.Compat;
 
-/// <summary>
-/// Adaptive Storage Framework by Soul, Phaneron, Bradson
-/// MP Compatibility patch
-/// </summary>
 [MpCompatFor("adaptive.storage.framework")]
 public class AdaptiveStorageFramework
 {
-    #region Fields
-
     private static FastInvokeHandler thingClassAnyFreeSlotsMethod;
-
-    #endregion
-
-    #region Init
 
     public AdaptiveStorageFramework(ModContentPack mod)
     {
@@ -38,94 +27,45 @@ public class AdaptiveStorageFramework
             .CancelIfAnyArgNull()
             .CancelIfNoSelectedMapObjects();
 
-        // Locate ThingClass
+        // Resolve ThingClass
         var type = AccessTools.TypeByName("AdaptiveStorage.ThingClass");
         if (type == null)
         {
-            Log.Warning("[MPCompat] ASF: Could not find AdaptiveStorage.ThingClass, skipping patches.");
+            Log.Warning("[MPCompat] ASF: ThingClass not found, skipping slot checks.");
             return;
         }
 
-        // Resolve AnyFreeSlots getter
         var getter = AccessTools.DeclaredPropertyGetter(type, "AnyFreeSlots");
         if (getter != null)
             thingClassAnyFreeSlotsMethod = MethodInvoker.GetHandler(getter);
         else
-            Log.Warning("[MPCompat] ASF: Missing AnyFreeSlots property.");
-
-        // Locate inner GodModeGizmos type
-        var inner = AccessTools.Inner(type, "GodModeGizmos");
-        if (inner == null)
-        {
-            Log.Warning("[MPCompat] ASF: Missing GodModeGizmos inner class.");
-            return;
-        }
-
-        MethodInfo lambdaMethod = null;
-
-        try
-        {
-            lambdaMethod = MpMethodUtil.GetLambda(
-                inner,
-                null,
-                MethodType.Constructor,
-                new[] { type },
-                0);
-        }
-        catch (Exception e)
-        {
-            Log.Warning("[MPCompat] ASF: Failed to get GodModeGizmos constructor lambda. Skipping dev sync.\n" + e);
-        }
-
-        // If the lambda is missing, skip patching
-        if (lambdaMethod == null)
-            return;
-
-        // Register dev-mode gizmo sync (debug only)
-        MP.RegisterSyncDelegate(inner, lambdaMethod.DeclaringType!.Name, lambdaMethod.Name, null)
-            .SetDebugOnly();
-
-        // Patch to prevent errors when storage is full
-        MpCompat.harmony.Patch(
-            lambdaMethod,
-            prefix: new HarmonyMethod(typeof(AdaptiveStorageFramework), nameof(CancelExecutionIfFull)));
+            Log.Warning("[MPCompat] ASF: AnyFreeSlots getter missing.");
     }
 
-    #endregion
-
-    #region Harmony patches
-
     [MpCompatPrefix("AdaptiveStorage.ContentsITab", "OnDropThing")]
-    private static bool CancelExecutionIfNotContained(ITab_ContentsBase __instance, Thing __0, ref int __1)
+    private static bool CancelExecutionIfNotContained(ITab_ContentsBase tab, Thing thing, ref int count)
     {
-        if (!__instance.container.Contains(__0))
+        if (!tab.container.Contains(thing))
             return false;
 
-        if (__0.stackCount < __1)
-            __1 = __0.stackCount;
+        if (thing.stackCount < count)
+            count = thing.stackCount;
 
         return true;
     }
 
-    private static bool CancelExecutionIfFull(Building_Storage ___Parent)
+    private static bool CancelExecutionIfFull(Building_Storage parent)
     {
         if (thingClassAnyFreeSlotsMethod == null)
             return true;
 
-        return (bool)thingClassAnyFreeSlotsMethod(___Parent);
+        return (bool)thingClassAnyFreeSlotsMethod(parent);
     }
-
-    #endregion
-
-    #region Sync workers
 
     [MpCompatSyncWorker("AdaptiveStorage.ContentsITab", shouldConstruct = true)]
     private static void NoSync(SyncWorker sync, ref object obj)
     {
-        // Only construct, do not sync
+        // Don't sync, only construct
     }
-
-    #endregion
 }
 
-   
